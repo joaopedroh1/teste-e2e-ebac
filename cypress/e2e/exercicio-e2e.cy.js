4ec8c7819ad47f0d1e48fc1f67b8c32c2fce539d
joao pedro <joaomhass4@gmail.com>
2024-08-15 21:24:58 -0300

    primeira alteração exercicio modulo 12

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -13,9 +13,20 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       cy.visit('/')
   });
 
-  it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
+  it('Deve fazer o login corretamente', () => {
+    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    cy.get('#username').type('teste.joao@teste.com.br')
+    cy.get('#password').type('kilerucv12')
+    cy.get('.woocommerce-form > .button').click()
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' ,'Olá, teste.joao (não é teste.joao? Sair)')
+  });
+   
+  it.only('Deve adicionar um produto no carrinho ', () => {
+    cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    cy.get('.post-2559 > .product-block > .caption > .meta > .infor > .name > a').click()
+    cy.get('.post-2559 > .product-block > .caption > .meta > .infor > .name > a').click()
+    cy.get('.post-2559 > .product-block > .caption > .meta > .infor > .name > a').click()
+    cy.get('.single_add_to_cart_button').click()
   });
 
 
