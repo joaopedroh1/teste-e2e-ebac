e79ee02cd97e027d45339563e5a29b2beceb8491
joao pedro <joaomhass4@gmail.com>
2024-08-28 20:00:58 -0300

    alteração de tudo - erro

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,5 +1,8 @@
 /// <reference types="cypress" />
 import { faker } from '@faker-js/faker';
+import joaoProdutosPage from '../support/page_objects/joao-produtos.page';
+const perfil = require('../fixtures/perfil.json')
+
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   /*  Como cliente 
@@ -10,33 +13,24 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       Preenchendo todas opções no checkout
       E validando minha compra ao final */
 
-  beforeEach(() => {
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-  });
-
-  afterEach(() => {
-    cy.screenshot()
-  });
-
-  it('Deve fazer o login corretamente', () => {
-    cy.get('#username').type('teste.joao@teste.com.br')
-    cy.get('#password').type('kilerucv12')
-    cy.get('.woocommerce-form > .button').click()
-    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' ,'Olá, teste.joao (não é teste.joao? Sair)')
+  it.only('Deve fazer o login corretamente - Usando Fixture', () => {
+    
+    cy.fixture('perfil').then(dados => {
+      cy.get('#username').type(dados.usuario)
+      cy.get('#password').type(dados.senha)
+      cy.get('.woocommerce-form > .button').click()
+      cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' ,'Olá, teste.joao (não é teste.joao? Sair)')
+    })
   });
 
   describe('Funcionalidade: Produtos', () => {
     beforeEach(() => {
-      cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+      produtosPage.buscarProdutos('')
     });
 
-    it.only('deve selecionar 4 produtos da lista ', () => {
-      cy.get('.product-block')
-        .eq(2)
-        .click()
-      cy.get('.product_title').should('contain' , 'Aether Gym Pant')
-
-
+    it('deve selecionar 4 produtos da lista ', () => {
+      joaoProdutosPage.visitarProduto
     });
+
   });  
 })
---
+++ cypress/support/page_objects/joao-produtos.page.js
@@ -0,0 +1,17 @@
+class ProdutosPage {
+
+    visitarUrl(){
+        cy.visit('produtos')
+    }
+
+    visitarProduto() {
+        
+    }
+
+    addProdutoCarrinho(){
+        
+    }
+
+}
+
+export default new ProdutosPage()
